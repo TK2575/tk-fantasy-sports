035775baf05eb2b2e0fd709c8b0f8a6b2b8901c7
Tom <tom.kain@gmail.com>
2021-12-21 22:56:34 -0500

    Init commit - POC auth to Yahoo fantasy sports API

---
+++ src/main/java/dev/tk2575/tkfantasysports/App.java
@@ -0,0 +1,13 @@
+package dev.tk2575.tkfantasysports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tk2575/tkfantasysports/AppTests.java
@@ -0,0 +1,13 @@
+package dev.tk2575.tkfantasysports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
